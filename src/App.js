1d31e0c88a8f53332a61e486fd33de7fbda7c8e3
nospam1961 <nospam1961-github@peaceduke.com>
2020-09-24 19:28:09 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br />Hello there! ... Changed.
+          <br />Hello there! ... changed on the feature branch.
         </p>
         <a
           className="App-link"
